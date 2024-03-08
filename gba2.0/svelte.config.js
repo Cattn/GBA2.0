88e5545e62eb296f974e285be9f27cbcb99db1fe
Logan <Cattn@mathstudy.dev>
2024-03-08 18:46:11 -0500

    cfpages

--- gba2.0/svelte.config.js
+++ gba2.0/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,12 +8,16 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		}),
 	},
 	paths: {
 		"$lib": ["./src/lib"],
 		"$lib/*": ["./src/lib/*"],
-		base:`/gba-svelte-beta`,
 	},
 	alias: {
 		lib: "src/lib",
