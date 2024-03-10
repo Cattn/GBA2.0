8d588b6f9a2dabf211b9d7f7505ada572b055288
Logan <Cattn@mathstudy.dev>
2024-03-09 20:25:39 -0500

    homepage

--- gba2.0/src/routes/+layout.svelte
+++ gba2.0/src/routes/+layout.svelte
@@ -2,8 +2,11 @@
 	import '../app.pcss';
 	import Nav from '$lib/components/Nav.svelte';
 	import { ModeWatcher } from "mode-watcher";
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 <ModeWatcher />
 <Nav/>
 
 <slot />
+
+<Footer/>
