6ef2d4c4ea4d703a75d44fa8a1dbde0fcd66d854
Logan <Cattn@mathstudy.dev>
2024-03-09 20:25:54 -0500

    homepagev2 (forgot to commit)

--- gba2.0/src/routes/+page.svelte
+++ gba2.0/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { AspectRatio } from "$lib/components/ui/aspect-ratio";
     import mainLogo from "$lib/images/GBA_2_main_2.png";
     let hovered = false;
 
@@ -23,13 +22,14 @@
     ];
 
     let randomTitle = possibleTitles[Math.floor(Math.random() * possibleTitles.length)];
+    import { Button } from "$lib/components/ui/button";
   </script>
 
-  <div class="flex align-center justify-center mt-12">
+  <div class="flex align-center justify-center mt-44">
     <img
       src={mainLogo}
       alt="Gray by Drew Beamer"
-      class="h-3/6 w-3/6 rounded-md object-cover"
+      class="h-1/4 w-1/4 rounded-md object-cover"
       class:hovered
       on:mouseenter={() => {
           hovered = true;
@@ -40,9 +40,16 @@
     />
   </div>
 
-  <h1 class="scroll-m-20 text-3xl font-bold tracking-tight lg:text-3xl mt-12 text-center italic">
-    {randomTitle}
+  <h1 class="scroll-m-20 text-3xl font-bold tracking-tight lg:text-3xl mt-4 text-center italic">
+    Ready to play?
   </h1>
+
+    <div class="flex justify-center mt-6">
+    <div class="flex gap-2">
+        <Button>Add a Library</Button>
+        <Button variant="secondary" disabled>Upload a Library</Button>
+    </div>
+    </div>
 
   <style>
     .hovered {
